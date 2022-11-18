fefe7c651543b93f7c37befd8bcfafa44d50073d
Dhernandez04 <deimercode@gmail.com>
2022-11-18 09:17:51 -0500

    Agregando proyecto

---
+++ src/main/java/com/dhernandez/puntodeventa/app/PuntodeventaApplication.java
@@ -0,0 +1,13 @@
+package com.dhernandez.puntodeventa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntodeventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntodeventaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhernandez/puntodeventa/app/PuntodeventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhernandez.puntodeventa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntodeventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
